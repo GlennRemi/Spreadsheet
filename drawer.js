13c35364f1027be00c041bdf518a003b237af5ff
GlennRemi <glennremi.kokdehode@gmail.com>
2022-11-10 14:27:28 +0100

    enabled celltracking, storage/collection of cell info, and party created basic calculation

--- drawer.js
+++ drawer.js
@@ -2,10 +2,16 @@ function drawing() {
   document.querySelector(".draw").addEventListener("click", (draw) => {
     const rowAmount = document.querySelector(".row");
     const collumAmount = document.querySelector(".collum");
+    const initiateBtn = document.querySelector(".draw");
     let rowInput = rowAmount.value;
     let collumInput = collumAmount.value;
-    document.body.innerHTML = "";
 
+    /* Cleaning up old DOM */
+    rowAmount.remove();
+    collumAmount.remove();
+    initiateBtn.remove();
+
+    /* Drawing up all cells */
     for (let indexRow = -1; indexRow < rowInput; indexRow++) {
       const lineBreak = document.createElement("div");
       lineBreak.classList.add("linebreak");
@@ -18,6 +24,7 @@ function drawing() {
         lineBreak.appendChild(collumList);
       }
     }
+
     /* Selecting borders and disabling them with number/letter ID for user*/
     const borderEdit = document.querySelectorAll("input");
     const collumIndex = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -44,6 +51,7 @@ function drawing() {
       borderEdit[borderIndex * rowSkip].placeholder = `${borderIndex}`;
       borderEdit[borderIndex * rowSkip].removeAttribute("class");
     }
+
     /* First border block */
     const firstBlock = document.querySelector(".grid--1--1");
     firstBlock.placeholder = "Spreadsheet";
@@ -52,9 +60,32 @@ function drawing() {
     firstBlock.disabled = true;
     firstBlock.removeAttribute("class");
 
-    /* Listner on cell */
+    /* Main Array with nested array with all data
+    Nested array 0= Cell-Cordinates
+    Nested array 1= Cell-Value unmodified
+    Nested array 2= Cell-Value calculated
+    */
     const cellTracker = document.querySelectorAll(".grid");
-    let cellValueArray = new Array(cellTracker.length);
+
+    let UsercellId = [[], [], []];
+
+    UsercellId[1] = new Array(cellTracker.length);
+    UsercellId[2] = new Array(cellTracker.length);
+
+    /* Detection array for detect cell-value-cordinates in calculation */
+    for (
+      let gridCollumIndex = 0;
+      gridCollumIndex < rowInput;
+      gridCollumIndex++
+    ) {
+      let gridRowNumber = gridCollumIndex + 1;
+      for (let gridRowIndex = 0; gridRowIndex < collumInput; gridRowIndex++) {
+        let gridCollumLetter = collumIndexArray[gridRowIndex];
+        UsercellId[0].push(gridCollumLetter + gridRowNumber);
+      }
+    }
+
+    /* Listner on cell and edits nesting array with inputdata */
     for (
       let userCellIndex = 0;
       userCellIndex < cellTracker.length;
@@ -63,7 +94,12 @@ function drawing() {
       cellTracker[userCellIndex].value;
       cellTracker[userCellIndex].addEventListener("keyup", function () {
         console.log(cellTracker[userCellIndex].value);
-        cellValueArray.splice(
+        UsercellId[1].splice(
+          userCellIndex,
+          1,
+          cellTracker[userCellIndex].value
+        );
+        UsercellId[2].splice(
           userCellIndex,
           1,
           cellTracker[userCellIndex].value
@@ -79,8 +115,7 @@ function drawing() {
     document
       .querySelector(".debugcheck")
       .addEventListener("click", function () {
-        console.log(`test + ${cellValueArray}`);
-        console.log(cellTracker.length);
+        console.log(UsercellId);
       });
 
     return cellTracker;
